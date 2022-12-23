73378ee45fd14e6666bb3b7e2ac0aad6af1b27a9
rendi <Rendipur234@gmail.com>
2022-12-23 12:40:15 +0700

    create pages team,role,employee. create layout sidebar

---
+++ layouts/form.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div class="pt-[50px] md:px-[50px] flex justify-between items-center px-4">
+        <nuxt-link to="/">
+            <img src="/assets/svgs/logo-type.svg" alt="">
+        </nuxt-link>
+        <a href="#" onclick="history.back()">
+            <img src="/assets/svgs/ric-close-white.svg" alt="">
+        </a>
+    </div>
+        <Nuxt />
+    </div>
+</template>
